4eb96665ab1ee3fbd43e098d39e4d83c397249a2
Rafael Krbashyan <rkrbashyan@teldev.io>
2025-08-04 19:27:13 +0300

    Setup Nuxt-3 with Pinia, Sass, Prettier

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <h2 @click="counterStore.increment">Welcome</h2>
+    <p>count is: {{ counterStore.count }}</p>
+    <p>doubleCount is: {{ counterStore.doubleCount }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore()
+</script>
